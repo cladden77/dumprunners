6689ea4828331aa94ce8553a048313ab01cd59a5
Chris L <chris.ladden@gmail.com>
2023-06-17 08:08:51 -0500

    initial commit

---
+++ resources/views/flexible-content/hero.blade.php
@@ -0,0 +1,13 @@
+{{-- Homepage Hero blade --}}
+
+<section class="hero" style="background: rgb(0,54,88);
+background: linear-gradient(270deg, rgba(0,54,88,0) 0%, rgba(0,54,88,1) 97%), url('@sub('hero_image')') center center/cover no-repeat; ">
+  <div class="row">
+    <div class="col-lg-10 col-md-10 col-sm-12 m-auto text-center text-md-left text-sm-center">
+      <div class="hero--content">
+        <h1 class="pl-0">@sub('hero_title')</h1>
+        <h2 class="pt-0 pl-0">@sub('hero_subtitle')</h2>
+      </div>
+    </div>
+  </div>
+</section>
---
+++ resources/views/flexible-content/listings.blade.php
@@ -0,0 +1,33 @@
+<section class="listings py-5" id="@sub('listings_id')">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="headline text-center">
+                <h2>@sub('headline')</h2>
+            </div>
+        </div>
+
+        <div class="row row-cols-1 row-cols-md-3 row-cols-sm-1 g-4 g-sm-2">
+            <?php if( have_rows('listings_repeater') ): ?>
+                <?php while (have_rows('listings_repeater')) : the_row(); ?>
+                        <div class="col-10 col-sm-10 col-md-4 my-3 mx-auto mx-md-0 m-sm-3">
+                        <div class="card h-100">
+                            <div class="card-img-wrapper">
+                            <img src="@sub('listings_image', 'url')" alt="@sub('listings_image', 'alt')" class="card-img-top img-fluid">
+                            </div>
+                            <div class="card-body">
+                            <h5 class="card-title">@sub('listing_title')</h5>
+                            <p class="listing-price">@sub('listing_price')</p>
+                            <p class="card-text">@sub('listing_description')</p>
+                            </div>
+                        </div>
+                        </div>
+                <?php endwhile; ?>
+            <?php endif; ?>
+        </div>
+        <div class="row">
+            <div class="col mt-5 text-center">
+                <a class="button button--primary" href="@sub('cta_button', 'url')">@sub('cta_button', 'title')</a>
+            </div>
+        </div>
+    </div>
+</section>
